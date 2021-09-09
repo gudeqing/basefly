c355bc63105c42a00afcdb2133197a2fe1711606
gudeqing <822466659@qq.com>
2021-09-09 15:47:54 +0800

    增加pair info参数

--- utils/get_fastq_info.py
+++ utils/get_fastq_info.py
@@ -1,21 +1,24 @@
+# coding=utf-8
 import os
 import re
 import json
+__author__ = 'gudeqing'
 
 
-def get_fastq_info(fastq_files: tuple = None, fastq_dirs: tuple = None, out='fastq.info.json',
-                   r1_name: str = "(.*).R1.fq.gz", r2_name: str = "(.*).R2.fq.gz",
+def get_fastq_info(fastq_files=None, fastq_dirs=None, pair_info=None, out='fastq.info.json',
+                   r1_name="(.*).R1.fq.gz", r2_name="(.*).R2.fq.gz",
                    link_data=False, add_s_to_numeric_name=False, middle2underscore=False):
     """
     :param fastq_files: target fastq file list. 'fastq_files' or 'fastq_dirs' must be provided.
     :param fastq_dirs: directory list, target fastq files should be in these directories. All target files in 'fastq_files' or 'fastq_dirs' will be used.
+    :param pair_info: 'pair info file that contains two columns without any header: [tumor_name, normal_name]
     :param r1_name: python regExp that describes the full name of read1 fastq file name. It requires at least one pair small brackets, and the string matched in the first pair brackets will be used as sample name. Example: '(.*).R1.fq.gz'
     :param r2_name: python regExp that describes the full name of read2 fastq file name. It requires at least one pair small brackets, and the string matched in the first pair brackets will be used as sample name. Example: '(.*).R2.fq.gz'
     :param link_data: bool to indicate if to make soft links for fastq files
     :param out: output file that contains three columns: [sample_name, read1_abs_path, read2_abs_path]
     :param add_s_to_numeric_name: bool value to indicate if to add a 'S' letter at the head of the sample name that startswith numeric string.
     :param middle2underscore: bool value to indicate if to transform '-' letter to '_' letter for a sample name.
-    :return: result_dict： {sample: [[r1, r1'], [r2, r2']], ...}
+    :return: result_dict: {sample: [[r1, r1'], [r2, r2']], ...}
     """
     if not (fastq_dirs or fastq_files):
         raise Exception("At least one of 'fastq_files' or 'fastq_dirs' must be provided.")
@@ -99,10 +102,24 @@ def get_fastq_info(fastq_files: tuple = None, fastq_dirs: tuple = None, out='fas
             json.dump(new_result, f, indent=2)
     else:
         with open(out, 'w') as f:
-            for k, v in new_result.items():
-                read1 = ';'.join(v[0])
-                read2 = ';'.join(v[1])
-                f.write(f'{k}\t{read1}\t{read2}\n')
+            if pair_info:
+                with open(pair_info) as fr:
+                    for line in fr:
+                        tumor, normal = line.strip().split()
+                        if tumor in new_result and normal in new_result:
+                            tr1 = ';'.join(new_result[tumor][0])
+                            tr2 = ';'.join(new_result[tumor][1])
+                            nr1 = ';'.join(new_result[normal][0])
+                            nr2 = ';'.join(new_result[normal][1])
+                            lst = [tumor, tr1, tr2, normal, nr1, nr2]
+                            f.write('\t'.join(lst)+'\n')
+                        else:
+                            print(f'{tumor} or {normal} fastq is not found !')
+            else:
+                for k, v in new_result.items():
+                    read1 = ';'.join(v[0])
+                    read2 = ';'.join(v[1])
+                    f.write('{k}\t{read1}\t{read2}\n'.format(k=k, read1=read1, read2=read2))
 
     return new_result
 
@@ -115,13 +132,14 @@ if __name__ == '__main__':
     parser.add_argument('-r1_name', required=True, help="python regExp that describes the full name of read1 fastq file name. It requires at least one pair small brackets, and the string matched in the first pair brackets will be used as sample name. Example: '(.*).R1.fq.gz'")
     parser.add_argument('-r2_name', required=True, help="python regExp that describes the full name of read2 fastq file name. It requires at least one pair small brackets, and the string matched in the first pair brackets will be used as sample name. Example: '(.*).R2.fq.gz'")
     parser.add_argument('-out', required=False, default='fastq.info.json', help='output file that contains three columns: [sample_name, read1_abs_path, read2_abs_path]')
+    parser.add_argument('-pair_info', required=False, help='pair info file that contains two columns without any header: [tumor, normal]')
     parser.add_argument('--add_s_to_numeric_name', action='store_true', default=False, help="if to add a 'S' letter at the head of the sample name that startswith numeric string.")
     parser.add_argument('--middle2underscore', action='store_true', default=False, help="if to transform '-' letter to '_' letter for a sample name")
     parser.add_argument('--link', action='store_true', default=False, help="if to transform '-' letter to '_' letter for a sample name")
     args = parser.parse_args()
     get_fastq_info(
         fastq_files=args.fastq_files, fastq_dirs=args.fastq_dirs,
-        r1_name=args.r1_name, r2_name=args.r2_name,
+        r1_name=args.r1_name, r2_name=args.r2_name, pair_info=args.pair_info,
         link_data=args.link, out=args.out,
         add_s_to_numeric_name=args.add_s_to_numeric_name,
         middle2underscore=args.middle2underscore
