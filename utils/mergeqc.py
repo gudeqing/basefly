2d3139dbcea3b3432337b1e54dc203bfb987fcea
gudeqing <822466659@qq.com>
2023-11-23 13:26:06 +0800

    增加fold80质控指标，调整输出格式

--- utils/mergeqc.py
+++ utils/mergeqc.py
@@ -12,25 +12,27 @@ def merge_umi_qc(
         prefix=None,
 ):
     """
-    用于umi数据分析流程的质控汇总：fastp的分析结果，bamdst的深度统计，umi统计
+    用于umi数据分析流程的质控汇总：fastp的分析结果，bamdst的深度统计，umi统计, 最好仅用于汇总一个样本的分析结果
     :param fastp_json_files: [json, json2], 根据文件名称信息提取样本名信息， sample_name = file_name.split('.')[0]
     :param : bamdst_cov_report_files:  第一个UMI处理前的，第二个是UMI处理后的; 需要和bamdst_depth_files一一对应,
     :param : bamdst_depth_files: 第一个UMI处理前的，第二个是UMI处理后的; 需要和bamdst_cov_report_files一一对应，
-    :param umi_family_size_files: [family_size, family_size2]
+    :param umi_family_size_files: [family_size, family_size2, ...], 根据文件名称信息提取样本名信息， sample_name = file_name.split('.')[0]
     :param outdir: 输出结果目录
-    :param prefix: 输出文件名前缀
+    :param prefix: 输出文件名前缀, 默认用样本名称作为前缀
     :return:
     """
     print('Merging QC table......')
     result = dict()
 
     # 汇总fastp信息
+    samples = []
     for stat_file in fastp_json_files:
         if not os.path.exists(stat_file):
             print(f'skip un-existing file {stat_file}')
             continue
         # 文件名用‘.‘分割后，取第一个值作为样本名称
         sample = os.path.basename(stat_file).split('.', 1)[0]
+        samples.append(sample)
         json_dict = json.load(open(stat_file))
         target_info = dict()
         # target_info['sequencing'] = json_dict['summary']['sequencing']
@@ -49,6 +51,7 @@ def merge_umi_qc(
 
     # 汇总bamdst的信息
     for idx, (stat_file, depth_file) in enumerate(zip(bamdst_cov_report_files, bamdst_depth_files)):
+        sample = samples[0] if len(samples) == 1 else samples[idx]
         if not os.path.exists(stat_file):
             print(f'skip un-existing file {stat_file}')
             continue
@@ -65,10 +68,12 @@ def merge_umi_qc(
                 target_info[name] = value
         # 计算均一性
         data = pd.read_table(depth_file, header=0, low_memory=False)
-        mean_coverage = data['Cover depth'].mean()
+        # And the "rmdup depth" is calculated after remove duplicated reads and secondary alignment
+        # reads and low map quality reads(mapQ < 20), this value is similar with the output depth of samtools depth.
+        mean_depth = data['Rmdup depth'].mean()
         panel_size = data.shape[0]
-        target_info['[Target] Coverage (>=0.2*MeanDepth)'] = sum(data['Rmdup depth'] >= mean_coverage*0.2)/panel_size
-        target_info['[Target] Coverage (>=0.5*MeanDepth)'] = sum(data['Rmdup depth'] >= mean_coverage*0.5)/panel_size
+        target_info['[Target] Coverage (>=0.2*MeanDepth)'] = sum(data['Rmdup depth'] >= mean_depth*0.2)/panel_size
+        target_info['[Target] Coverage (>=0.5*MeanDepth)'] = sum(data['Rmdup depth'] >= mean_depth*0.5)/panel_size
         target_info['[Target] Coverage (>=200x)'] = sum(data['Rmdup depth'] >= 200)/panel_size
         target_info['[Target] Coverage (>=300x)'] = sum(data['Rmdup depth'] >= 300)/panel_size
         target_info['[Target] Coverage (>=500x)'] = sum(data['Rmdup depth'] >= 500)/panel_size
@@ -76,11 +81,12 @@ def merge_umi_qc(
         target_info['[Target] Coverage (>=2000x)'] = sum(data['Rmdup depth'] >= 2000)/panel_size
         target_info['[Target] Coverage (>=5000x)'] = sum(data['Rmdup depth'] >= 5000)/panel_size
         target_info['[Target] Coverage (>=10000x)'] = sum(data['Rmdup depth'] >= 10000)/panel_size
+        target_info['[Target] Fold80BasePenalty'] = mean_depth/data['Rmdup depth'].quantile(0.2)
         # 假设第一组数据是UMI处理前的统计结果，第二组数据是UMI consensus后的结果，加上标签以示区别
         if idx == 0:
-            target_info = {'[Raw]'+ key: value for key, value in target_info.items()}
+            target_info = {'[Raw]' + key: value for key, value in target_info.items()}
         else:
-            target_info = {'[Consensus]'+ key: value for key, value in target_info.items()}
+            target_info = {'[Consensus]' + key: value for key, value in target_info.items()}
         # 样本名称沿用前面fastp文件名中提取的，因为bamdst文件名是固定的，不包含样本名信息
         if sample in result:
             result[sample].update(target_info)
@@ -119,44 +125,52 @@ def merge_umi_qc(
     table.to_excel(os.path.join(outdir, f'{prefix}.QC.all.metrics.xlsx'))
 
     # 精简QC报告
-    target_rows = [
-        "Total_raw_bases(G)",
-        "Total_raw_reads(M)",
-        "GC_content",
-        "[Raw][Target] Average depth",
-        "[Consensus][Target] Average depth",
-        "Insert_size_peak",
-        "[Raw][Total] Fraction of Mapped Reads",
-        "Q20_rate",
-        "Q30_rate",
-        "[Raw][Target] Fraction of Target Data in all data",
-        "[Raw][Target] Coverage (>=0.2*MeanDepth)",
-        "[Raw][Target] Coverage (>=0.5*MeanDepth)",
-        "[Raw][flank] Fraction of flank Reads in all reads",
-        "[Raw][Target] Coverage (>=200x)",
-        "[Raw][Target] Coverage (>=500x)",
-        "[Raw][Target] Coverage (>=2000x)",
-        "[Raw][Target] Coverage (>=5000x)",
-        "[Consensus][Target] Fraction of Target Data in all data",
-        "[Consensus][Target] Coverage (>=0.2*MeanDepth)",
-        "[Consensus][Target] Coverage (>=0.5*MeanDepth)",
-        "[Consensus][flank] Fraction of flank Reads in all reads",
-        "[Consensus][Target] Coverage (>=200x)",
-        "[Consensus][Target] Coverage (>=500x)",
-        "[Consensus][Target] Coverage (>=2000x)",
-        "[Consensus][Target] Coverage (>=5000x)",
-        "UmiFamilySize=1:count",
-        "UmiFamilySize=2:count",
-        "UmiFamilySize=3:count",
-        "UmiFamilySize=1:fraction",
-        "UmiFamilySize=2:fraction",
-        "UmiFamilySize>=2:fraction",
-        "UmiFamilySize=3:fraction",
-        "UmiFamilySize>=3:fraction",
-    ]
-    target_rows = [x for x in target_rows if x in table.index]
-    table.loc[target_rows].to_excel(os.path.join(outdir, f'{prefix}.QC.target.metrics.xlsx'))
-    table.loc[target_rows].to_csv(os.path.join(outdir, f'{prefix}.QC.target.metrics.txt'), sep='\t')
+    target_metrics = {
+        "Total_raw_bases(G)": "TotalRawBasesGb",
+        "Total_raw_reads(M)": "TotalRawReadsMb",
+        "GC_content": "GCRatio",  # report
+        "Duplication(sequence-based)": "DupRate",  # report
+        "[Raw][Target] Average depth": "RawMeanDepth",
+        "[Consensus][Target] Average depth": "UmiMeanDepth",  # report
+        "Insert_size_peak": "InsertSize",  # report
+        "[Raw][Total] Fraction of Mapped Reads": "MappingRate",  # report
+        "Q20_rate": "Q20",
+        "Q30_rate": "Q30",  # report
+        "[Raw][Target] Fraction of Target Data in all data": "OnTargetRate",  # report
+        "[Raw][Target] Coverage (>=0.2*MeanDepth)": "Uniformity20",
+        "[Raw][Target] Coverage (>=0.5*MeanDepth)": "Uniformity50",
+        "[Raw][Target] Fold80BasePenalty": "Fold80BasePenalty",  # report
+        "[Raw][flank] Fraction of flank Reads in all reads": "OnFlankRate",
+        "[Raw][Target] Coverage (>=200x)": "RawDepthOver200x",
+        "[Raw][Target] Coverage (>=500x)": "RawDepthOver500x",
+        "[Raw][Target] Coverage (>=2000x)": "RawDepthOver2000x",
+        "[Raw][Target] Coverage (>=5000x)": "RawDepthOver5000x",
+        "[Consensus][Target] Fraction of Target Data in all data": "UmiOnTargetRate",
+        "[Consensus][Target] Coverage (>=0.2*MeanDepth)": "UmiUniformity20",
+        "[Consensus][Target] Coverage (>=0.5*MeanDepth)": "UmiUniformity50",  # report
+        "[Consensus][flank] Fraction of flank Reads in all reads": "UmiOnFlankRate",
+        "[Consensus][Target] Coverage (>=200x)": "UmiDepthOver200x",
+        "[Consensus][Target] Coverage (>=500x)": "UmiDepthOver500x",
+        "[Consensus][Target] Coverage (>=1000x)": "UmiDepthOver1000x",  # report
+        "[Consensus][Target] Coverage (>=2000x)": "UmiDepthOver2000x",
+        "[Consensus][Target] Coverage (>=5000x)": "UmiDepthOver5000x",
+        "UmiFamilySize=1:count": "UmiFamilySize1Count",
+        "UmiFamilySize=2:count": "UmiFamilySize2Count",
+        "UmiFamilySize=3:count": "UmiFamilySize3Count",
+        "UmiFamilySize=1:fraction": "UmiFamilySize1Percent",
+        "UmiFamilySize=2:fraction": "UmiFamilySize2Percent",
+        "UmiFamilySize=3:fraction": "UmiFamilySize3Percent",
+        "UmiFamilySize>=2:fraction": "UmiFamilySizeOver2Percent",  # report
+        "UmiFamilySize>=3:fraction": "UmiFamilySizeOver3Percent",  # report
+    }
+    target_rows = [x for x in target_metrics.keys() if x in table.index]
+    table = table.loc[target_rows]
+    table.index = [x for x in target_metrics.values()]
+    table.to_excel(os.path.join(outdir, f'{prefix}.QC.target.metrics.xlsx'))
+    table = table.T.round(4)
+    table.index.name = 'Sample'
+    table.to_csv(os.path.join(outdir, f'{prefix}.QC.target.metrics.txt'), sep='\t')
+    table.to_csv(os.path.join(outdir, f'{prefix}.QC.target.metrics.csv'))
 
 
 if __name__ == '__main__':
