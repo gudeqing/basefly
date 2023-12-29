26c8de087f8fd8fe7db905970982cb13874542d1
gudeqing <822466659@qq.com>
2023-12-29 12:08:36 +0800

    调整筛选策略

--- microhap/microhapdb_screen.py
+++ microhap/microhapdb_screen.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import gzip
 from collections import Counter
+from itertools import combinations
 
 
 """
+# microhapdb
 https://microhapdb.readthedocs.io/en/latest/citations.html#published-marker-collections-and-allele-frequency-data
+
+# 该数据库包含人群信息如下：
+----------------------------------------------------------------------------
 ID	Name	Source	翻译
 MHDBP-936bc36f79	Asia	vanderGaag2018	亚洲（不对）
 ChengduHan	Chengdu Han	Zou2022	成都汉族
@@ -28,28 +33,46 @@ mMHseq-TWChinese	TWChinese	Gandotra2020	台湾
 WuzhongHui	Wuzhong Hui	Zou2022	中国
 XichangYi	Xichang Yi	Zou2022	中国
 ZunyiGelao	Zunyi Gelao	Zou2022	中国
+----------------------------------------------------------------------------
 
-在千人基因组中:
-    Super Population Code	中文注释
-    EAS (东亚人群)	CHB：北京汉族；JPT：东京日本人；CHS：南方汉族；CDX：傣族；KHV：越南京族
-    EUR (欧洲人群)	CEU：犹他州白人；TSI：意大利托斯卡纳人；FIN：芬兰人；GBR：英国英格兰和苏格兰人；IBS：西班牙伊比利亚人
-    AFR (非洲人群)	YRI：尼日利亚约鲁巴人；LWK：肯尼亚卢希亚人；GWD：冈比亚西非人；MSL：塞拉利昂门德人；ESN：尼日利亚埃塞俄比亚人；ASW：美国非洲裔美国人；ACB：巴巴多斯非洲加勒比海人
-    AMR (拉丁美洲人群)	MXL：墨西哥裔美国人；PUR：波多黎各人；CLM：哥伦比亚哥伦比亚人；PEL：秘鲁利马人
-    SAS (南亚人群)	GIH：印度古吉拉特邦人；PJL：巴基斯坦旁遮普邦人；BEB：孟加拉国人；STU：斯里兰卡泰米尔人；ITU：英国印度泰卢固邦人
-    
-注意：
-1. 这里有不少群体没有对应的frequency信息，可能是信息无法获取
+# 在千人基因组中:
+Super Population Code	中文注释
+EAS (东亚人群)	CHB：北京汉族；JPT：东京日本人；CHS：南方汉族；CDX：傣族；KHV：越南京族
+EUR (欧洲人群)	CEU：犹他州白人；TSI：意大利托斯卡纳人；FIN：芬兰人；GBR：英国英格兰和苏格兰人；IBS：西班牙伊比利亚人
+AFR (非洲人群)	YRI：尼日利亚约鲁巴人；LWK：肯尼亚卢希亚人；GWD：冈比亚西非人；MSL：塞拉利昂门德人；ESN：尼日利亚埃塞俄比亚人；ASW：美国非洲裔美国人；ACB：巴巴多斯非洲加勒比海人
+AMR (拉丁美洲人群)	MXL：墨西哥裔美国人；PUR：波多黎各人；CLM：哥伦比亚哥伦比亚人；PEL：秘鲁利马人
+SAS (南亚人群)	GIH：印度古吉拉特邦人；PJL：巴基斯坦旁遮普邦人；BEB：孟加拉国人；STU：斯里兰卡泰米尔人；ITU：英国印度泰卢固邦人
+
+# 筛选策略思考：
+1. 数据库中有不少群体没有对应的frequency信息，猜测是从文献中获得，文献中没有提供相应数据
 2. 筛选人群频率比较高的位点，比如>5%
 3. 避免频繁突变和频繁重组
 4. 很明显，无数个等位基因可以最大程度区分任何两个2个体，即两两之间的等位基因都不同
 5. 等位基因之间的出现频率比较接近时，在检查随机混合物时，区分度越好，比如2个等位基因，各自人群频率为50%最具备区分度
-6. 有多个位点，但等位基因的分布频率相差较大，该如何对位点进行排序
-7. 经典人群遗传学给出的额答案是计算有效等位基因数量Ae: effective number of alleles
-   如，二等位基因Ae = 1/(p^2+q^2)
+6. 有多个位点，但等位基因的分布频率相差较大，该如何对位点进行排序，筛选时可能需要考虑这一点
+7. 经典人群遗传学给出的额答案是计算有效等位基因数量Ae: effective number of alleles 如，二等位基因Ae = 1/(p^2+q^2)
 8. 如果我们过于强调高平均AE的选择，我们可能会忽视等位基因频率在地理区域之间的巨大差异，从而选择较少的祖先信息基因座。
+9. 位于可以高度置信地测序的基因组区域中
+10. 建议MRD和chimerism在同一个panel中实现？ctDNA一般有富集的实验过程，可能让嵌合率的结果失真？
+11. 筛选多少个marker？https://www.sciencedirect.com/science/article/pii/S0009898122011792
+    With guidelines suggesting to monitor at least three informative markers,
+    we demonstrate that, for optimized assays, at least 40 biallelic markers need to be screened to achieve enough informative markers in over 99% of cases.
+    根据这篇文献，为覆盖99%的情况，建议至少40个双等位基因marker，每次实验中，要求至少存在3个有效marker。
+    通常，将markerhap视为一个整体的话，有不少是3等位以上的，因此可能需要的marker数量可能更少。
+12. https://pubmed.ncbi.nlm.nih.gov/33582770/这篇文献中，使用了84对距离10bp以内的且连锁不平衡的SNP对，作为造血干细胞移植后检测的marker，检测下限：0.09%， 定量下限（相对背景噪音）0.39%
+13. 基于之前microhap分析的实战经验，发现2个基因型如果仅仅相差一个碱基，而且C->T, G->A之间的转换，容易因为测序错误引来偏差或噪音
+14. 我们可能需要尽量避免测序错误容易发生的位点，比如前后有3个以上的串联碱基重复的位点
+15. 筛选标准，筛选40个以上的marker：
+    0. 考虑捕获探针的长度一般为80-120nt
+    1. 长度（第一个snp和最后一个snp的距离）：25-150nt
+    2. 理论有效等位基因数量Ae: >= 2
+    3. 为减少测序错误带来的定量偏差，每个snp前后不能出现超过3个及以上的碱基串联重复
+    4. 每个microhap包含[2, 10]个变异位点
+    6. 为减少测序错误带来的定量偏差，对于一个microhap，需要谨慎考虑基因型之间相差的碱基仅仅是{C>T} 或 {G>A}的情形
 
-了解microhapdb
-验证数据库中Ae的计算，以'mh01CP-007'为例，基于1KGP人群，计算其Ae
+
+# 进一步了解microhapdb：
+## 验证数据库中Ae的计算，以'mh01CP-007'为例，基于1KGP人群，计算其Ae
 找人群频率：
     $ grep 'mh01CP-007' <(less frequency.csv.gz) |grep 1KGP
     mh01CP-007,1KGP,A|A|A,0.00115,5196,Byrska-Bishop2022
@@ -66,8 +89,46 @@ ZunyiGelao	Zunyi Gelao	Zou2022	中国
 验证是否相等：
     1.3297759816974677 ~= 1.330
 
+# 了解NGS的探针引物
+在整个NGS实验流程中，会用到4种NGS引物，分别为接头引物、多重PCR引物、封阻引物和杂交捕获探针
+1. 接头引物
+接头引物的作用是将待测DNA与测序仪建立联系，其本质是一段碱基序列，长度15-80 nt，主要结构可以分为3部分：
+a) P5/P7：是与Flow-cell上面寡核苷酸接头相同或互补的片段，能将待测DNA样本固定到测序芯片泳道上；
+b) SP1/SP2：测序时测序引物结合部分，是测序启动的位点；
+c) Index：用于区分不同样本，每个样本的Index都是唯一的；双端标签接头指的在两端均存在Index，能区分更多样本
+d) DNA Insert：待测DNA序列，一般长度为200-300 bp，不会超过1 kb。
+
+2. 封阻引物
+捕获测序中阻止捕获探针与文库接头区的杂交，降低捕获探针的脱靶效应，提高捕获测序的特异性，
+封阻引物长度一般根据靶DNA序列和接头区域序列相似度进行定制合成。
+
+3. 杂交捕获探针
+杂交捕获探针长度一般为80-120nt，它的工作原理是根据DNA碱基互补配对原则，其序列与靶序列互补且有生物素标记。
+在捕获探针与NGS文库进行杂交后，探针会与靶序列结合，然后使用链霉素磁珠富集杂交后的DNA文库，
+经过洗脱即可获得待测序列的文库。
+
+4. 多重PCR（Multiplex PCR）
+在多重PCR中，可以使用多个不同的引物，使多个目标序列在同一反应中被扩增。
+多重PCR引物长度一般为15-120 nt，设计时需要注意引物之间的特异性和互补性，以避免引物间相互干扰和非特异性扩增。
+多重PCR的优点包括可以在同一反应中扩增多个目标序列，减少反应时间和成本，同时还可以避免样本不足或失效的问题，
+目前已经应用到基因分型、遗传育种、生育健康、肿瘤检测于NGS靶向富集等多个领域。
+
+# microhap数据库的Ae信息
+In [3]: ae = pd.read_csv('marker-aes.csv')
+In [6]: ae['Population'].unique()
+Out[6]:  发现包含AE信息的marker涉及人群只有如下这些
+array(['1KGP', 'ACB', 'AFR', 'ASW', 'BEB', 'CDX', 'CEU', 'CHB', 'CHS',
+       'CLM', 'EAS', 'ESN', 'EUR', 'FIN', 'GBR', 'GIH', 'GWD', 'IBS',
+       'ITU', 'JPT', 'KHV', 'LWK', 'MSL', 'MXL', 'PEL', 'PJL', 'PUR',
+       'SAS', 'STU', 'TSI', 'YRI'], dtype=object)
+@和我们感兴趣的人群取一个交集
+In [7]: set(target_popultation_ids ) & set(ae_pops)
+Out[7]: {'CDX', 'CHB', 'CHS', 'EAS', 'JPT', 'KHV'}
+@也就是说，只有千人基因组计划的人群包含了Ae信息
 """
 # 根据人群名称信息，筛选得到如下候选
+from pysam import FastaFile
+genome = FastaFile('/home/hxbio04/biosofts/MicroHapulator/microhapulator/data/hg38.fasta')
 target_popultation_ids = [
     # 'JPT',  # 日本东京人，归为EAS
     # 'KHV',  # 越南, 归为EAS
@@ -94,33 +155,6 @@ target_popultation_ids = [
     'ZunyiGelao'   # 中国
 ]
 
-
-# marker_pop_ae_value_dict = dict()
-# with open('microhapdb/marker-aes.csv') as f:
-#     header = f.readline()
-#     for line in f:
-#         marker, pop, ae = line.strip().split(',')
-#         ae = float(ae)
-#         # 提取目标人群信息，筛选Ae >= 2的
-#         if pop in target_popultation_ids:
-#             if ae >= 2:
-#                 marker_pop_ae_value_dict[marker+'+'+pop] = ae
-
-"""
-@全局检查数据库
-In [3]: ae = pd.read_csv('marker-aes.csv')
-In [6]: ae['Population'].unique()
-Out[6]:  发现包含AE信息的marker涉及人群只有如下这些
-array(['1KGP', 'ACB', 'AFR', 'ASW', 'BEB', 'CDX', 'CEU', 'CHB', 'CHS',
-       'CLM', 'EAS', 'ESN', 'EUR', 'FIN', 'GBR', 'GIH', 'GWD', 'IBS',
-       'ITU', 'JPT', 'KHV', 'LWK', 'MSL', 'MXL', 'PEL', 'PJL', 'PUR',
-       'SAS', 'STU', 'TSI', 'YRI'], dtype=object)
-@和我们感兴趣的人群取一个交集
-In [7]: set(target_popultation_ids ) & set(ae_pops)
-Out[7]: {'CDX', 'CHB', 'CHS', 'EAS', 'JPT', 'KHV'}
-@也就是说，只有千人基因组计划的人群包含了Ae信息
-"""
-
 # 统计哪些目标群体没有AE信息可得
 # hit_pops = {x.split('+')[1] for x in marker_pop_ae_value_dict.keys()}
 # print('没有AE信息的群体有：', set(target_popultation_ids) - hit_pops)
@@ -136,9 +170,60 @@ Out[7]: {'CDX', 'CHB', 'CHS', 'EAS', 'JPT', 'KHV'}
 # target_freq_df.to_csv('target.freq.txt', index=False)
 
 
+def genotypes_is_distinguishable(allele_lst):
+    # 检查基因型两两之间的差异
+    # 假设recipient基因型（A-T-C | A-T-T），而donar基因型（A-T-C | A-T-C)
+    # 由于C容易测序错误为T，最后得到的定量误差会增加，因此我们尽量避免选用此类marker
+    # 当前，我们选定比较松弛的限制即easy_mix_num>=2才考虑丢弃
+    good = True
+    easy_mix_num = 0
+    for each, other in combinations(allele_lst, 2):
+        diff_bases = [(x, y) for x, y in zip(each, other) if x != y]
+        if len(diff_bases) == 1:
+            if sorted(diff_bases[0]) in [['C', 'T'], ['A', 'G']]:
+                easy_mix_num += 1
+    if easy_mix_num >= 2:
+        good = False
+    return good
+
+
+def near_very_short_tandem(row, repeat_len=3):
+    # 段串联重复区域容易测错，如AAAT有可能测成AAAA, 因此我们也希望能避免此类marker
+    NumVars, Extent, Chrom, Start, End, Positions, Positions37, RSIDs, Source, *_ = row
+    positions = [int(x) for x in Positions.split(';')]
+    discard = False
+    for pos in positions:
+        up_3bases = genome.fetch(Chrom, pos - repeat_len - 1, pos - 1)
+        down_3bases = genome.fetch(Chrom, pos, pos + repeat_len)
+        # 如果参考序列有小写的，则认为处于低复杂度区域
+        in_low_complex_region = any([x.islower() for x in up_3bases + down_3bases])
+        if len(set(up_3bases)) == 1 or len(set(down_3bases)) == 1 or in_low_complex_region:
+            discard = True
+    return discard
+
+
+def get_snp_context(row, extend=10):
+    NumVars, Extent, Chrom, Start, End, Positions, Positions37, RSIDs, Source, *_ = row
+    positions = [int(x) for x in Positions.split(';')]
+    contexts = []
+    for pos in positions:
+        up_3bases = genome.fetch(Chrom, pos - extend - 1, pos - 1)
+        current_base = genome.fetch(Chrom, pos-1, pos)
+        down_3bases = genome.fetch(Chrom, pos, pos + 10)
+        contexts.append(up_3bases + f'({current_base})' + down_3bases)
+    return ";".join(contexts)
+
+
 # 实际上，根据频率表可以计算出每一个marker的AE信息
 # 经验算，marker-aes.csv这张表是可以根据这个频率计算表获得，如下：
 freq = pd.read_csv('microhapdb/frequency.csv.gz', header=0)
+
+# 提取marker的在1KGP中的基因型信息
+marker_1KGP_info = freq.loc[freq['Population'] == '1KGP']
+distinguishable_info = marker_1KGP_info.groupby(['Marker', 'Population'])['Allele'].apply(genotypes_is_distinguishable).reset_index()
+marker_genotype_distinguishable = dict(zip(distinguishable_info['Marker'], distinguishable_info['Allele']))
+
+# 提取目标人群的信息，并计算有效等位基因数量Ae
 freq = freq.loc[[x in target_popultation_ids for x in freq['Population']]]
 ae_df = freq.groupby(['Marker', 'Population', 'Source'])['Frequency'].apply(lambda freqs: 1/sum(x**2 for x in freqs))
 ae_df.name = 'Ae'
@@ -151,34 +236,69 @@ target_markers = ae_df.reset_index()['Marker'].unique()
 marker_df = pd.read_csv('microhapdb/marker.csv', header=0, index_col=0)
 # 有些marker居然不在marker信息表
 missed_markers = [x for x in target_markers if x not in marker_df.index]
-print(f'有些marker居然不在marker信息表, {missed_markers}')
+print(f'frequency中有些marker居然不在marker信息表, {missed_markers}')
 target_markers = [x for x in target_markers if x in marker_df.index]
 target_marker_df = marker_df.loc[list(target_markers)]
 
-# 计算Ae平均值, 即计算千人基因组中的EAS人群，然后加15个其他人群的信息
-mean_ae = ae_df.reset_index().groupby('Marker')['Ae'].mean().round(3)
-target_marker_df['meanAe'] = mean_ae.loc[target_markers]
-target_marker_df = target_marker_df.sort_values(by='meanAe', ascending=False)
+# 计算Ae平均值, 即计算千人基因组中的EAS人群，然后加15个文献来源的其他人群信息
+mean_ae_df = ae_df.reset_index().groupby('Marker')['Ae'].mean().round(3)
+max_ae_df = ae_df.reset_index().groupby('Marker')['Ae'].max().round(3)
+min_ae_df = ae_df.reset_index().groupby('Marker')['Ae'].min().round(3)
+target_marker_df['meanAe'] = mean_ae_df.loc[target_marker_df.index]
+target_marker_df['maxAe'] = max_ae_df.loc[target_marker_df.index]
+target_marker_df['minAe'] = min_ae_df.loc[target_marker_df.index]
+target_marker_df = target_marker_df.sort_values(by='maxAe', ascending=False)
 
 # 过滤只包含一个snp的marker
 idx = target_marker_df['NumVars'] == 1
 print(f'过滤掉{sum(idx)}个只包含一个snp的marker, 他们是 {list(target_marker_df.loc[idx].index)}')
-target_marker_df = target_marker_df.loc[~idx]
-print(target_marker_df.describe())
-target_marker_df.to_csv('target_markers.csv')
+# target_marker_df = target_marker_df.loc[~idx]
+# target_marker_df.to_csv('target_markers.csv')
+min_len = 25
+max_len = 200
+min_ae = 2
+min_NumVars = 2
+max_NumVars = 7
+filter_criterion = [min_len, max_len, min_NumVars, max_NumVars]
 
-max_len = 100
-min_len = 50
-min_ae = 3
-max_NumVars = 10
+init_marker_num = target_marker_df.shape[0]
+print('起始marker数量', init_marker_num)
 target_marker_df = target_marker_df.loc[target_marker_df['Extent'] <= max_len]
 target_marker_df = target_marker_df.loc[target_marker_df['Extent'] >= min_len]
-target_marker_df = target_marker_df.loc[target_marker_df['meanAe'] >= min_ae]
+len_filtered_num = init_marker_num - target_marker_df.shape[0]
+print('进一步根据长度过滤掉的marker数量:', len_filtered_num)
+
+target_marker_df = target_marker_df.loc[target_marker_df['maxAe'] >= min_ae]
+ae_filtered_num = init_marker_num - len_filtered_num - target_marker_df.shape[0]
+print('进一步根据minAe过滤掉的marker数量:', ae_filtered_num)
+
+target_marker_df = target_marker_df.loc[target_marker_df['NumVars'] >= min_NumVars]
 target_marker_df = target_marker_df.loc[target_marker_df['NumVars'] <= max_NumVars]
-print(f'我们挑选: ({min_len} < extent < {max_len}) & (meanAe >={min_ae}) & (NumVars < {max_NumVars})的marker')
+numvars_filtered_num = init_marker_num - len_filtered_num - ae_filtered_num - target_marker_df.shape[0]
+print('进一步根据SNP数量过滤掉的marker数量:', numvars_filtered_num)
+
+# 根据marker的序列特征过滤
+target_marker_df = target_marker_df.loc[~target_marker_df.apply(near_very_short_tandem, axis=1)]
+complex_filtered_num = init_marker_num - len_filtered_num - ae_filtered_num - numvars_filtered_num - target_marker_df.shape[0]
+print('进一步根据SNP位点前后特征过滤掉的marker数量', complex_filtered_num)
+
+target_marker_df = target_marker_df.loc[[marker_genotype_distinguishable[x] for x in target_marker_df.index]]
+genotype_filtered_num = init_marker_num - len_filtered_num - ae_filtered_num - numvars_filtered_num - complex_filtered_num - target_marker_df.shape[0]
+print('进一步根据基因型之间是否容易因为测序错误易混淆而过滤掉的marker数量', genotype_filtered_num)
+
+# 增加每个marker的snp的前后碱基信息
+target_marker_df['SnpContext'] = target_marker_df.apply(get_snp_context, axis=1)
+
+# 总结
+print(f'Total {target_marker_df.shape[0]} markers left')
 print(target_marker_df.describe())
-target_marker_df.to_csv(f'target_markers.max{max_len}bp.minAe{min_ae}.csv')
+target_marker_df.to_csv(f'target.markers.csv')
+print('marker snp 数量分布统计：')
 print(target_marker_df['NumVars'].value_counts())
+print('marker chrome 数量分布统计')
+print(target_marker_df['Chrom'].value_counts())
+print('marker source 来源分布统计')
+print(target_marker_df['Source'].value_counts())
 
 # 提取通过筛选的marker的人群Ae信息
 target_marker_freq = freq.loc[[x in target_marker_df.index for x in freq['Marker']]]
@@ -191,35 +311,4 @@ target_df.to_csv('target.marker.zero-based.txt', sep='\t', index=False)
 
 # 其次想到的数据库是国内的女娲基因组资源, 我们依据这个资源可以对这些marker进行验算和重新排序，得到中国更准确的中国人群信息
 # http://bigdata.ibp.ac.cn/NyuWa_variants/search.php
-# 通过上述网址可以逐一验证位点信息
-"""
-FLT3
-KIT
-RAS
-PNPN11
-JAK2
-CBL
-PML
-RARA
-RUNX1
-RUNX1T1
-CBFB-MYH11
-MLL
-CEBPA
-NPM1
-DNMT3a
-TET2
-IDH1
-IDH2
-ASXL1
-WT1
-"""
-with open('allsample.txt') as f:
-    lines = f.readlines()
-    lines = [x for x in lines if x.strip()]
-    for i in range(0, len(lines)+1, 6):
-        samplename = lines[i].split()[1]
-        accession = lines[i+5].split()[1]
-        print(samplename + '\t' + accession)
-
-
+# NGS数据模拟
